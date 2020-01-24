4b47769af42f66c7c9a0914399e4076ba87ee697
oliviergf <olivier.gauthier.farley@hotmail.com>
2020-01-24 13:54:57 -0500

    adds MAE and some other things

--- intro/lesson1.py
+++ intro/lesson1.py
@@ -1,4 +1,7 @@
 # Code you have previously used to load data
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split
+from learntools.machine_learning.ex4 import *
 from learntools.machine_learning.ex3 import *
 from learntools.core import binder
 from sklearn.tree import DecisionTreeRegressor
@@ -60,3 +63,58 @@ step_4.check()
 
 # You can write code in this cell
 print(home_data.head())
+
+# //////////////////////////////////////////////////////////////////////////////////////////////////
+
+# Code you have previously used to load data
+
+# Path of the file to read
+iowa_file_path = '../input/home-data-for-ml-course/train.csv'
+
+home_data = pd.read_csv(iowa_file_path)
+y = home_data.SalePrice
+feature_columns = ['LotArea', 'YearBuilt', '1stFlrSF',
+                   '2ndFlrSF', 'FullBath', 'BedroomAbvGr', 'TotRmsAbvGrd']
+X = home_data[feature_columns]
+
+# Specify Model
+iowa_model = DecisionTreeRegressor()
+# Fit Model
+iowa_model.fit(X, y)
+
+print("First in-sample predictions:", iowa_model.predict(X.head()))
+print("Actual target values for those homes:", y.head().tolist())
+
+# Set up code checking
+binder.bind(globals())
+print("Setup Complete")
+
+
+# Import the train_test_split function and uncomment
+
+# fill in and uncomment
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
+
+# Check your answer
+step_1.check()
+
+
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
+
+
+# Specify the model
+iowa_model = DecisionTreeRegressor(random_state=1)
+
+# Fit iowa_model with the training data.
+iowa_model.fit(train_X, train_y)
+
+# Check your answer
+step_2.check()
+
+val_predictions = iowa_model.predict(val_X)
+
+
+val_mae = mean_absolute_error(val_y, val_predictions)
+
+# uncomment following line to see the validation_mae
+print(val_mae)
